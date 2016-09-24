8d17d38e0d598cef1134e08d7dc33955dae7445e
iliangogov <iliangogov@gmail.com>
2016-09-24 19:20:54 +0300

    HQClasses HW

---
+++ Topics/08. High-quality-Classes/homework/Abstraction/Models/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Abstraction.Contracts;
+
+namespace Abstraction.Models
+{
+    public class Circle : Figure, ICircle
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return this.radius;
+            }
+
+            set
+            {
+                var isValid = base.CheckIfInputValueIsValid(value);
+                if (!isValid)
+                {
+                    throw new ArgumentException("Radius");
+                }
+
+                this.radius = value;
+            }
+        }
+
+        public override double CalcPerimeter()
+        {
+            double perimeter = 2 * Math.PI * this.Radius;
+            return perimeter;
+        }
+
+        public override double CalcSurface()
+        {
+            double surface = Math.PI * this.Radius * this.Radius;
+            return surface;
+        }
+    }
+}
---
+++ Topics/08. High-quality-Classes/homework/Abstraction/Models/Figure.cs
@@ -0,0 +1,39 @@
+﻿using Abstraction.Contracts;
+
+namespace Abstraction.Models
+{
+    public abstract class Figure : IFigure
+    {
+        private const string ToStringTemplate = "I am a {2}. My perimeter is {0:f2}. My surface is {1:f2}.";
+
+        protected Figure()
+        {
+        }
+
+        public abstract double CalcPerimeter();
+
+        public abstract double CalcSurface();
+
+        public override string ToString()
+        {
+            var output = string.Format(
+                 Figure.ToStringTemplate,
+                 this.CalcPerimeter(),
+                 this.CalcSurface(),
+                 this.GetType().Name);
+
+            return output;
+        }
+
+        protected virtual bool CheckIfInputValueIsValid(double value)
+        {
+            var valueIsValid = true;
+            if (value <= 0)
+            {
+                valueIsValid = false;
+            }
+
+            return valueIsValid;
+        }
+    }
+}
